756456b6153d1e857fc65036f354857977d3fdfb
TeamRadioRoll <42188785+TeamRadioRoll@users.noreply.github.com>
2020-02-29 11:22:00 +0100

    Debug Shapes Now Green and Rectagle Uses Proper Cords

--- main.lua
+++ main.lua
@@ -1,16 +1,22 @@
-function function love.load()
+function love.load()
+  --Making MostImportant Variables
+
+  Player1 = {Cordsx = 0, Cordsy = 0}
+
+  --Defining the center of the game field
+  OriginPointX, OriginPointY = love.graphics.getPixelDimensions()
+  OriginPointX = OriginPointX / 2
+  OriginPointY = OriginPointY / 2
+end
+
+function love.update(dt)
 
 
 end
 
-function function love.update(dt)
+function love.draw()
+  --Makes Debug Shapes Green as they shoud never be seen ingame
+  love.graphics.setColor(0, 1, 0, 1)
 
-
-
-end
-
-function function love.draw()
-
-  
-
+  love.graphics.rectangle("fill",OriginPointX ,OriginPointY.Cordsy + Player1.Cordsx , 100, 100)
 end
